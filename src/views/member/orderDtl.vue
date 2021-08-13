400bbe0cd80a5de045de2047f363b1d364f7a129
king <king_w318@163.com>
2021-08-13 17:14:55 +0800

    修改订单详情物流信息

--- src/views/member/orderDtl.vue
+++ src/views/member/orderDtl.vue
@@ -317,7 +317,9 @@
 </script>
 <style lang="scss" scoped>
   @import "~@/styles/variables.scss";
-
+	.el-carousel__item {
+	    overflow-y: auto!important;
+	}
   .ly-container {
     font-size: 14px;
 
--- src/views/order/list.vue
+++ src/views/order/list.vue
@@ -605,7 +605,9 @@
 </script>
 <style lang="scss" scoped>
   @import "~@/styles/variables.scss";
-
+	.el-carousel__item {
+	    overflow-y: auto!important;
+	}
   .ly-container {
     font-size: 14px;
 
