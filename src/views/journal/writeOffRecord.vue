9000126bfa5b1464f86a312be7b4ab3bd795ef06
lack_kong <1825609765@qq.com>
2022-08-31 18:28:18 +0800

    1.添加核销记录

--- src/views/journal/writeOffRecord.vue
+++ src/views/journal/writeOffRecord.vue
@@ -5,26 +5,46 @@
         <div class="tabTd">
           <div>订单编号：</div>
           <div>
-            <el-input v-model="searchParam.orderNo" placeholder="请输入" width="180px" />
+            <el-input v-model="searchParam.searchOrderNo" placeholder="请输入" width="180px" />
           </div>
         </div>
         <div class="tabTd">
-         <el-button icon="el-icon-search" @click="search()">
-           搜索
-         </el-button>
+          <div>注册手机号：</div>
+          <div>
+            <el-input v-model="searchParam.searchRegisterPhoneNo" placeholder="请输入" width="180px" />
+          </div>
+        </div>
+        <div class="tabTd">
+          <el-button icon="el-icon-search" @click="search()">
+            搜索
+          </el-button>
         </div>
       </div>
       <div class="ly-table-panel">
         <div class="ly-data-list">
           <el-table ref="mainTable" :data="tableData.list" style="width: 100%; margin-bottom: 20px;" row-key="index"
-            border>
+                    border>
             <el-table-column prop="orderNo" label="订单编号" />
-            <el-table-column prop="testPhone" label="买家手机号" />
+            <el-table-column prop="registerPhoneNo" label="注册手机号" />
+            <el-table-column prop="exchangeType" label="核销方式">
+              <template slot-scope="scope">
+                <span v-if="scope.row.exchangeType == 1">管理后台</span>
+                <span v-if="scope.row.exchangeType == 2">App</span>
+              </template>
+            </el-table-column>
+            <el-table-column prop="orderDate" label="下单时间" />
+            <el-table-column prop="exchangeDate" label="核销时间" />
+            <el-table-column prop="exchangeStoreName" label="核销门店" />
+            <el-table-column prop="exchangeAccount" label="核销账号" />
+            <el-table-column prop="searchOrderNo" label="搜索订单编号" />
+            <el-table-column prop="searchRegisterPhoneNo" label="搜索注册手机号" />
+            <el-table-column prop="searchPhoneNo" label="搜索卖家手机号" />
+
           </el-table>
         </div>
         <div class="ly-data-pagination">
           <el-pagination v-show="!showPagination" :current-page="searchParam.pageNum"  :total="tableData.total" background layout="prev, pager, next"
-            @current-change="currentPage" @prev-click="currentPage" @next-click="currentPage" />
+                         @current-change="currentPage" @prev-click="currentPage" @next-click="currentPage" />
         </div>
       </div>
     </div>
@@ -44,7 +64,9 @@
         loading: true,
         searchParam: {
           pageSize: 10,
-          pageNum: 1
+          pageNum: 1,
+          searchOrderNo:"",
+          searchRegisterPhoneNo: "",
         },
         tableData: {
           list: []
@@ -72,12 +94,12 @@
       },
       loadList() {
         const scope = this
-          getMethod('/permission/search-operation-list', this.searchParam).then(res => {
-            scope.tableData.list = res.data.records
-            scope.tableData.total = res.data.total
-            scope.showPagination =  res.data.total == 0
-            this.loading = false
-          })
+        getMethod('/order/order-exchange-record-list', this.searchParam).then(res => {
+          scope.tableData.list = res.data.records
+          scope.tableData.total = res.data.total
+          scope.showPagination =  res.data.total == 0
+          this.loading = false
+        })
       }
     }
   }
